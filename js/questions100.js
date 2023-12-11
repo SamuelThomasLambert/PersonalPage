a3b0a98db49b84e6db97ea178e99ddb0d4ae4142
SamuelThomasLambert <149679445+SamuelThomasLambert@users.noreply.github.com>
2023-12-11 12:50:28 +0900

    Add files via upload

--- js/questions100.js
+++ js/questions100.js
@@ -1,11 +1,44 @@
 // Content of Questions100.js
-const csvData = `Animals,Sports,Jobs,Food,Feelings,Appearance,Comparatives,Activities,Communication,Emotion,Movement
-nouns,nouns,nouns,nouns,adjectives,adjectives,adjectives,verbs,verbs,verbs,verbs
-elephant,soccer,plumber,apple,happy,green,slower,cook,talk,love,walk
-chicken,tennis,firefighter,orange,sad,beautiful,bigger,swim,tell,hate,run 
-dog,basketball,architect,banana,surprised,ugly,stronger,drive,ask,enjoy,sprint
-cat,cricket,lawyer,pizza,angry,pretty,shorter,read,answer,wonder,jump
-horse,golf,scientist,grapes,bored,tall,longer,study,speak,dislike,skip
-zebra,volleyball,nurse,burger,excited,blue,younger,exercise,write,admire,gallop
-lion,badminton,engineer,watermelon,confused,purple,older,meditate,communicate,hug,climb
+const csvData = `Animals,Sports,Jobs,Food,Feelings,Appearance,Comparatives,Activities,Communication,Emotion,Movement,places,Countries,Vehicles,Clothing,Body Parts,Instruments,Nature,.
+nouns,nouns,nouns,nouns,adjectives,adjectives,adjectives,verbs,verbs,verbs,verbs,nouns,nouns,nouns,nouns,nouns,nouns,nouns,.
+dog,soccer,plumber,apple,happy,green,slower,cook,talk,love,walk,library ,Japan,Car,tshirt,arm,guitar,tree,.
+cat,tennis,firefighter,orange,sad,beautiful,bigger,swim,tell,hate,run ,park,Australia,Bus,dress,leg,piano,grass,.
+giraffe,basketball,architect,banana,surprised,ugly,stronger,drive,ask,enjoy,sprint,valley,Germany,Bicylcle,pants,eye,trumpet,flower,.
+lion,cricket,lawyer,pizza,angry,pretty,shorter,read,answer,wonder,jump,museum,Spain,Motorcycle,jacket,hand,flute,mountain,.
+chicken,golf,scientist,grapes,bored,tall,longer,study,speak,dislike,skip,canyon,Italy,Scooter,shorts,foot,drum,lake,.
+goat,volleyball,nurse,burger,excited,blue,younger,exercise,write,admire,gallop,lake,Canada,Truck,skirt,arm,guitar,desert,.
+monkey,badminton,engineer,watermelon,confused,purple,older,meditate,communicate,hug,climb,island,France,Boat,sweater,leg,violin,beach,.
+elephant,rugby,pilot,strawberry,relaxed,yellow,smarter,play,listen,cherish,crawl,hill,Brazil,Bicycle,tie,eye,accordion,mountain,.
+zebra,baseball,teacher,carrot,frustrated,gray,quicker,dance,observe,appreciate,skip,hall,India,Carriage,dress,hair,trumpet,forest,.
+penguin,swimming,artist,pear,shocked,pink,calmer,sing,understand,comfort,slide,cafe,Japan,Convertible,helmet,hand,piano,sky,.
+tiger,hockey,doctor,grapefruit,curious,orange,faster,sleep,express,awe,leap,forest,Australia,Car,scarf,foot,flute,valley,.
+horse,running,developer,mango,content,black,cleaner,create,inquire,delight,galloping,market,Mexico,SUV,hoodie,eye,harp,river,.
+koala,surfing,writer,blueberry,anxious,red,calmest,explore,share,joyful,stroll,glade,Brazil,Motorcycle,sunglasses,leg,accordion,field,.
+dolphin,snowboarding,photographer,kiwi,silly,brown,happier,observe,connect,nostalgic,swim,park,Canada,Bus,gloves,arm,violin,lake,.
+rhinoceros,skiing,actor,pineapple,embarrassed,white,braver,teach,convey,inspire,charge,cavern,France,Bicycle,sweater,hand,drum,meadow,.
+deer,cycling,scientist,pomegranate,thoughtful,striped,finer,travel,express,admire,saunter,glacier,Mexico,Convertible,hat,leg,flute,valley,.
+gazelle,football,painter,melon,lonely,striped,wilder,discover,converse,appreciate,roam,island,Italy,Carriage,tie,hair,trumpet,coast,.
+lemur,badminton,engineer,blueberry,surprised,yellow,calmest,observe,share,happy,climb,harbor,Spain,Truck,gloves,foot,guitar,beach,.
+snake,squash,dentist,kiwi,ecstatic,shiny,larger,jump,write,adore,crawl,jungle,Germany,Scooter,sunglasses,arm,harp,desert,.
+batminton,surfing,programmer,pear,frustrated,dark,calmer,read,communicate,love,slide,canyon,Brazil,Convertible,jacket,leg,violin,meadow,.
+panda,archery,teacher,blueberry,hopeful,tiny,smoother,dance,talk,appreciate,skip,peak,Japan,Bus,tshirt,hair,trumpet,sky,.
+wolf,swimming,artist,pomegranate,serene,colorful,quicker,swim,sing,enjoy,jump,lake,Italy,Scooter,sunglasses,foot,accordion,field,.
+cheetah,volleyball,nurse,banana,thoughtful,gray,longer,run,express,comfort,stroll,valley,Spain,Car,scarf,hand,flute,river,.
+chameleon,badminton,firefighter,grapes,bored,plain,stronger,study,ask,admire,sprint,hill,India,Motorcycle,jacket,arm,guitar,forest,.
+tiger,tennis,plumber,pizza,silly,glowing,shorter,write,tell,hate,run,,,Convertible,,eye,,meadow,.
+giraffe,cricket,lawyer,watermelon,happy,beautiful,bigger,drive,listen,love,climb,,,,,,,coast,.
+elephant,golf,pilot,carrot,angry,tall,older,meditate,speak,delight,crawl,,,,,,,river,.
+zebra,soccer,scientist,mango,excited,green,slower,cook,enjoy,dislike,gallop,,,,,,,,.
+penguin,hockey,engineer,orange,content,pretty,faster,study,talk,wonder,jump,,,,,,,,.
+lion,running,developer,apple,curious,pink,younger,exercise,ask,awe,sprint,,,,,,,,.
+gazelle,cycling,actor,kiwi,anxious,striped,happier,meditate,write,hug,slide,,,,,,,,.
+deer,basketball,photographer,blueberry,surprised,yellow,calmest,explore,share,nostalgic,saunter,,,,,,,,.
+koala,swimming,doctor,strawberry,confused,red,cleaner,create,connect,joyful,climb,,,,,,,,.
+dolphin,snowboarding,writer,grapefruit,lonely,black,wilder,sleep,observe,admire,run,,,,,,,,.
+rhinoceros,skiing,painter,pineapple,embarrassed,striped,finer,dance,inquire,delight,roam,,,,,,,,.
+lemur,football,actor,burger,shocked,ugly,braver,play,express,appreciate,climb,,,,,,,,.
+snake,swimming,plumber,pear,elated,shiny,skinnier,study,speak,love,walk,,,,,,,,.
+batminton,squash,firefighter,orange,hopeful,tiny,calmer,run,ask,hate,slide,,,,,,,,.
+panda,basketball,teacher,kiwi,ecstatic,plain,slower,cook,talk,enjoy,jump,,,,,,,,.
+wolf,tennis,lawyer,blueberry,sad,colorful,shorter,swim,tell,dislike,stroll,,,,,,,,.
 `
