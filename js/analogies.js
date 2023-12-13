2054c25010b0daffb73cea93cc22c5ff73987e7c
SamuelThomasLambert <149679445+SamuelThomasLambert@users.noreply.github.com>
2023-12-13 12:35:41 +0900

    Add files via upload

---
+++ js/analogies.js
@@ -0,0 +1,41 @@
+    const csvData = `wind is to {breeze}, storm is to {hurricane}, gentle gust is to {typhoon}, zephyr is to {light wind}, tornado is to {whirlwind}
+        bicycle is to {pedals}, car is to {accelerator}, skateboard is to {push}, scooter is to {kick}, boat is to {oars}
+        whisper is to {quiet}, Shout is to {loud}, murmur is to {soft}, scream is to {piercing}, mumble is to {indistinct}
+        feather is to {light}, stone is to {heavy}, paper is to {lightweight}, cotton is to {fluffy}, helium balloon is to {buoyant}
+        river is to {flowing}, pond is to {still}, waterfall is to {cascading}, stream is to {babbling}, ocean is to {surging}
+        clock is to {seconds}, calendar is to {days}, stopwatch is to {milliseconds}, hourglass is to {minutes}, sundial is to {sunlight}
+        seed is to {plant}, egg is to {bird}, spore is to {fungus}, acorn is to {oak tree}, bulb is to {flower}
+        puzzle is to {solve}, mystery is to {unravel}, riddle is to {decipher}, maze is to {navigate}, enigma is to {comprehend}
+        smile is to {happiness}, tear is to {sadness}, laughter is to {joy}, frown is to {displeasure}, grin is to {amusement}
+        pen is to {writing}, keyboard is to {typing}, pencil is to {sketching}, marker is to {highlighting}, brush is to {painting}
+        fire is to {warmth}, ice is to {cold}, sunlight is to {radiance}, shadow is to {shade}, candle flame is to {flicker}
+mountain is to {elevation}, valley is to {depression}, plateau is to {flat}, canyon is to {depth}, hill is to {slope}
+telescope is to {observe}, microscope is to {examine}, binoculars is to {scan}, periscope is to {survey}, camera is to {capture}
+rainbow is to {colorful}, monochrome is to {black and white}, spectrum is to {multicolored}, gradient is to {shaded}, prism is to {refraction}
+novel is to {prose}, poem is to {verse}, drama is to {dialogue}, essay is to {argument}, haiku is to {syllables}
+dance is to {movement}, sculpture is to {form}, painting is to {color}, music is to {sound}, literature is to {expression}
+chef is to {cooking}, bartender is to {mixing}, baker is to {baking}, barista is to {brewing}, sommelier is to {pairing}
+detective is to {investigate}, lawyer is to {litigate}, judge is to {adjudicate}, juror is to {deliberate}, witness is to {testify}
+athlete is to {physical}, scholar is to {intellectual}, artist is to {creative}, entrepreneur is to {innovative}, scientist is to {analytical}
+conductor is to {direct}, composer is to {compose}, musician is to {perform}, singer is to {sing}, dancer is to {move}
+rain is to {wet}, snow is to {cold}, sleet is to {icy}, hail is to {pellets}, drizzle is to {light}
+forest is to {dense}, desert is to {sparse}, tundra is to {barren}, jungle is to {lush}, savanna is to {grassy}
+galaxy is to {vast}, constellation is to {patterned}, comet is to {streaking}, asteroid is to {rocky}, planet is to {orbital}
+laptop is to {portable}, desktop is to {stationary}, tablet is to {compact}, smartphone is to {handheld}, server is to {centralized}
+handshake is to {formal}, hug is to {affectionate}, high-five is to {casual}, fist bump is to {informal}, salute is to {respectful}
+pyramid is to {ancient}, skyscraper is to {modern}, castle is to {medieval}, hut is to {primitive}, mansion is to {opulent}
+kite is to {soaring}, glider is to {gliding}, hot air balloon is to {floating}, airplane is to {flying}, rocket is to {propelling}
+earthquake is to {tremor}, tsunami is to {tidal wave}, hurricane is to {whirlwind}, tornado is to {twister}, avalanche is to {snowslide}
+rainbow is to {after rain}, sunset is to {evening}, sunrise is to {morning}, eclipse is to {obscured}, dusk is to {twilight}
+microscope is to {tiny}, telescope is to {distant}, magnifying glass is to {enlarged}, binoculars is to {zoomed}, periscope is to {observed}
+telescope is to {observe}, microscope is to {examine}, binoculars is to {scan}, periscope is to {survey}, camera is to {capture}
+virus is to {infection}, bacteria is to {microorganism}, fungi is to {mycelium}, parasite is to {host}, protozoa is to {single-celled}
+handshake is to {greeting}, nod is to {acknowledgment}, wave is to {farewell}, hug is to {affection}, salute is to {respect}
+mountain is to {summit}, valley is to {lowland}, peak is to {pinnacle}, canyon is to {gorge}, hill is to {crest}
+city is to {urban}, suburb is to {residential}, village is to {rural}, metropolis is to {cosmopolitan}, town is to {local}
+rain is to {wet}, snow is to {cold}, hail is to {icy}, drizzle is to {light}, downpour is to {heavy}
+marathon is to {endurance}, sprint is to {speed}, relay is to {teamwork}, hurdles is to {obstacles}, long jump is to {distance}
+symphony is to {orchestra}, quartet is to {four}, solo is to {one}, ensemble is to {group}, concerto is to {soloist}
+detective is to {investigation}, lawyer is to {litigation}, judge is to {adjudication}, juror is to {deliberation}, witness is to {testimony}
+novel is to {prose}, poem is to {verse}, drama is to {dialogue}, essay is to {argument}, screenplay is to {script}
+rose is to {fragrant}, lily is to {elegant}, sunflower is to {radiant}, daisy is to {simple}, orchid is to {exotic}`;
