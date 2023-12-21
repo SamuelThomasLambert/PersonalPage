33079f5ea78f83c4b486d30c73b9fc398e5738d0
SamuelThomasLambert <149679445+SamuelThomasLambert@users.noreply.github.com>
2023-12-21 14:11:31 +0900

    Add files via upload

---
+++ js/wolfQuestions.js
@@ -0,0 +1,25 @@
+function getwolfQuestions() {
+    return [
+        ['What was your nickname in school?','What is an adjective to describe your pet?'],
+        ['How many times do you drink water in a day?','How many times do you check your phone in a day?'],
+        ['Whats the weirdest thing yous ever eaten?', 'If you had to eat an insect or animal that people dont usually eat, what would you eat?'],
+        ['Whats the most embarrassing song on your playlist?', 'What is your least favorite song?'],
+        ['If you could only wear one color for all your clothes, what color would you choose?', 'What color do you avoid wearing?'],
+        ['Have you ever accidently called someone mom or dad? who was it?', 'Who is somebody you know who you adimre?'],
+        ['What do you put in coffee?', 'What is the strongest drink you have ever tried?'],
+        ['What cartoon / anime character do you look like?', 'Whos your favorite cartoon / anime character?'],
+        ['If you were to buy a gift for someone in this room, what would you buy?', 'Whats your favorite type of flower?'],
+        ['If you could invent a new flavor of chocolate, what would it be?', 'What is your favorite seafood?'],
+        ['Whats your favorite type of dessert?', 'What is something you find too sweet?'],
+        ['What is the biggest animal you think you could beat in a fight?','What is the scariest animal?'],
+        ['What is your favorite pizza topping?','What is your favorite fruit?'],
+        ['What is something you mum used to always ask you?','If animals could talk, what would you ask your pet?'],
+        ['How many grapes can you keep in your mouth without chewing?','How many coins can you fit in your pocket?'],
+        ['How many balls can you juggle?','How many balls do you own?'],
+        ['What is the most illigal or bad thing you have done?','If you were arrested for no reason, what would your friends think you have done?'],
+        ['Where is the most unusual place you have fallen asleep?','What is the most uncomfrotable place you have tried to sit?'],
+        ['If you had a secret hiding place in your house, what would you keep in there?','What is the weirdest gift you have ever recieved?'],
+        ['What is a bad smell you kind of like?','What is the worst smell?'],
+        ['If you had to change your name, what would you change it to?','What is good name for a pet?']
+]; 
+}     
